f85ec16cdf9621c5cc0093da78f397e74a22825e
hjcenry <359583129@qq.com>
2019-11-11 09:41:31 +0800

    修改查询的商品

--- capture.py
+++ capture.py
@@ -1,8 +1,8 @@
 from config import Config
-from capture_service.sony_camera_service import SonyCameraService
+from sony_a7m3_service import SonyA7M3Service
 
 config = Config('config.ini')
 
 if __name__ == '__main__':
-    sony_cam_capture = SonyCameraService(config)
+    sony_cam_capture = SonyA7M3Service(config)
     sony_cam_capture.run()
